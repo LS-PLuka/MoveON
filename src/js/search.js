4e1764747a8a4b76004fdb000a1927d5590b287a
Pedro Luka <pedrolukagt@gmail.com>
2025-03-20 17:53:34 -0300

    Trabalho em equipe geral

---
+++ src/js/search.js
@@ -0,0 +1,9 @@
+// Barra de Pesquisa
+function toggleSearchBar() {
+    const searchBar = document.getElementById('searchBar');
+    if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+        searchBar.style.display = 'block';
+    } else {
+        searchBar.style.display = 'none';
+    }
+}
