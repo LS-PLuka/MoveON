5d8849b251cf9970435bb84e84debc3870062e9c
Pedro Luka <pedrolukagt@gmail.com>
2025-03-21 14:09:57 -0300

    Criação da Barra de Pesquisa no Index

--- src/js/search.js
+++ src/js/search.js
@@ -1,8 +1,9 @@
 // Barra de Pesquisa
 function toggleSearchBar() {
     const searchBar = document.getElementById('searchBar');
+    
     if (searchBar.style.display === 'none' || searchBar.style.display === '') {
-        searchBar.style.display = 'block';
+        searchBar.style.display = 'flex';
     } else {
         searchBar.style.display = 'none';
     }
